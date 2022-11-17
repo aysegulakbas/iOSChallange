6ac655f7836b798920c88683f1064218b102d409
Ayşegül AKBAŞ <aysegulakbaas@gmail.com>
2022-11-17 20:14:44 +0300

    commit
    commit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Darwin
 
 var date = Date()
 let dateFormatter = DateFormatter()
@@ -32,3 +33,17 @@ print("Kadın Gunleri Sayısı \(results.count)")
 //Dict içinde bulunan kadın öğrencileri filtrele.
 
 
+
+
+struct Student {
+  var name = ""
+  var age = 0
+  var gender = 0 //0 male, 1 female so you can random it
+  var schoolNumber = 0
+}
+let student = Student(name: "Ad", age: Int.random(in: 18...25), gender: Int.random(in: 0...1), schoolNumber: Int.random(in: 0...1000))
+
+struct Class {
+    var students: [Student] = []
+    var capacity: Int = 0
+}
--- ObjectOriented4.playground/Contents.swift
+++ ObjectOriented4.playground/Contents.swift
@@ -34,3 +34,12 @@ dateFormatter.dateFormat = "dd.MM.YYYY"
 let dataText = dateFormatter.string(from: date)
 print(dataText)
 
+// Extension kullanimi
+
+extension Double {
+    var km:Double {return self*1000.0}
+    var m:Double {return self}
+    var cm:Double {return self/100.0}
+    var mm:Double {return self/1000.0}
+}
+print("10 cm \(10.cm)")
