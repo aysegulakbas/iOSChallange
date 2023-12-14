ce8593a45316751e116776428c928adbfbe29e11
aysegulakbas <aysegulakbaas@gmail.com>
2023-12-14 19:35:24 +0300

    variadic parameters

--- Swift Functions.playground/Contents.swift
+++ Swift Functions.playground/Contents.swift
@@ -76,3 +76,47 @@ m.ext(num1: 10.0, num2: 20.0)
 m.min(num1: 30, num2: 10)
 print(m.imp(num1: 22, num2: 12))
 m.summ_(num1: 3, num2: 2)
+
+// Overloading : Metotlarin asiri yuklenmesi ayni isimde birden fazla fonksiyonun bulunmasi
+/* Isimler ayni fakat degiskenler farkli*/
+
+class Accounting {
+    func summ (num1: Int, num2: Int){
+        print("Sum : \(num1 + num2)")
+    }
+    
+    func summ (num1: Double, num2: Int){
+        print("Sum : \(num1 + Double(num2))")
+    }
+    
+    func summ (num1: Int, num2: Double){
+        print("Sum : \(Double(num1) + num2)")
+    }
+    
+    func summ (num1: Int, num2: Int, name:String){
+        print("Sum is name \(name) : Result:  \(num1 + num2)")
+    }
+}
+
+var a = Accounting()
+
+a.summ(num1: 10, num2: 20)
+a.summ(num1: 12, num2: 13, name: "Sum")
+
+// Variadic Parameters
+/* Variadic parametreler demek, birden fazla sayi ile ani islemi yapmak istedigimizde kullaniriz.*/
+
+func sumVariadic(numbers:Int...) -> Int{
+    var sum = 0
+    for s in numbers {
+        sum = sum + s
+    }
+    return sum
+}
+
+var v1 = sumVariadic(numbers: 1,2,3,4,5)
+var v2 = sumVariadic(numbers: 17,256,45,6789,23)
+print(v1)
+print(v2)
+
+// 58.video
