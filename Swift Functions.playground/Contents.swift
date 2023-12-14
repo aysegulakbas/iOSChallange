6e4368ad5288fed8dcdac230b8c7c131577f6466
aysegulakbas <aysegulakbaas@gmail.com>
2023-12-14 16:19:17 +0300

    Swift Functions

---
+++ Swift Functions.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+// Not included no return Functions
+
+func greeting() {
+    let hello = "Hello Everyone!"
+    print(hello)
+}
+greeting() // I called to greeting function. Just do procces.
+
+//  Included return Functions
+
+func greeting_() -> String {
+    let hello = "Hello Everyone!"
+    return hello
+}
+
+let result = greeting()
+print(result)
+
+// Parameter included funtions 5.22
+
+
