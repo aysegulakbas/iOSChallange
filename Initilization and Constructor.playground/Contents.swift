e52b751c79f72395d2e34228e62c7a369bf1c120
aysegulakbas <aysegulakbaas@gmail.com>
2023-12-16 17:14:33 +0300

    init and const

---
+++ Initilization and Constructor.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class People {
+    var name : String
+    var age : Int
+    
+    init(name:String, age:Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+var p = People(name: "Ayse", age: 10)
+
+// Class and Struct Difference video 70
+
+
