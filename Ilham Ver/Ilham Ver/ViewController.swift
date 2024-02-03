6b600c03b0dd16fb046ad1f1b92f16717cbb431a
aysegulakbas <aysegulakbaas@gmail.com>
2024-02-03 18:58:00 +0300

    completed

---
+++ Ilham Ver/Ilham Ver/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Ilham Ver
+//
+//  Created by aysegul on 3.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func button(_ sender: Any) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
